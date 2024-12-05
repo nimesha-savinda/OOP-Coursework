a5ce9d9083ffde446e8ac9e46efe76d941570039
Nimesha Savinda Dahanayaka <159873731+nimesha-savinda@users.noreply.github.com>
2024-12-05 08:32:00 +0530

    input validation for ticket track loop

--- Backend/src/main/java/org/oop/backend/CLI/CLI.java
+++ Backend/src/main/java/org/oop/backend/CLI/CLI.java
@@ -16,17 +16,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 //import java.lang.module.Configuration;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class CLI {
 
     public static void main(String[] args) {
-        System.setProperty("logging.level.root", "OFF");
+        System.setProperty("logging.level.root", "OFF");  //to turn of the cli loggers
         System.setProperty("logging.level.org.springframework.boot", "OFF");
         System.setProperty("logging.level.org.springframework.web", "OFF");
 
         ConfigurableApplicationContext context = SpringApplication.run(BackendApplication.class, args);
-
         CustomerController Customer_Controller = context.getBean(CustomerController.class);
         VendorController Vendor_Controller = context.getBean(VendorController.class);
         TicketPoolController ticketPoolController= context.getBean(TicketPoolController.class);
@@ -35,26 +35,26 @@ public class CLI {
         ConfigController configController=context.getBean(ConfigController.class);
         ConfigurationRepository configurationRepository=context.getBean(ConfigurationRepository.class);
         TicketPoolRepository ticketPoolRepository = context.getBean(TicketPoolRepository.class);
+        //to import the properties from the backend
 
+        Boolean cont = true; // the boolean variable to run the loop
 
-        Boolean cont = true;
+        Scanner input = new Scanner(System.in);//for the inputs of the system
 
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("1");
+        System.out.println("Welcome to the ticket system..! :)");
 
         while(cont) {
-            System.out.println("*****************\n" +
+            System.out.println("*****************\n" +  //Main menu
                                 "1)Create Configuration File\n"+
                                 "2)View Configuration File\n"+
                                 "3)Track Ticket Pool\n"+
                                 "4)Customer Panel\n" +
                                 "6)Quit\n"+
                                 "*****************");
-            String value = input.nextLine();
+            String value = input.nextLine();  //this variable will be checked below for find the selected option
             switch (value) {
                 case "1":
-                    System.out.println("Total_number_of_tickets:");
+                    System.out.println("Total_number_of_tickets:");//asks for variables to create the config file
                     Integer Total_number_of_tickets = input.nextInt();
                     System.out.println("Ticket_release_rate:");
                     Integer Ticket_release_rate = input.nextInt();
@@ -71,27 +71,24 @@ public class CLI {
                     ticketPoolRepository.save(new_ticketPool);
                     //This creates a new ticket pool along with the configuration file for the new session
 
-
                     break;
 
                 case "2":
-                        Configuration configuration1 = configController.getLatest();
+                        Configuration configuration1 = configController.getLatest();//Shows the current config file
                         System.out.println("-------Current Configuration Settings-------" +
                                 "           \nTotal number of Tickets: "+configuration1.getTotal_number_of_tickets()+
                                             "\nTicket Release rate: "+configuration1.getTicket_release_rate()+
                                             "\nRetrieval Rate: "+configuration1.getRetrieval_rate()+
                                             "\nMaximum Capacity: "+configuration1.getMaximum_Capacity()+
                                             "\n-------------------------------------------");
-
-
                     break;
                 case "3":
 
-                    Boolean ticket_pool_loop = true;
+                    Boolean ticket_pool_loop = true; //this variable is used to run the ticket tracking loop
 
                     while(ticket_pool_loop){
 
-                        for(int i = 0;i<5;i++){
+                        for(int i = 0;i<5;i++){//after 5 iterations it will ask to continue or not
                             TicketPool ticketPool = ticketPoolController.getTheLatestTicketPool();
                             System.out.println("++++++++++++++++\n" +
                                                 "Available Tickets: "+ticketPool.getAvailableTickets()+
@@ -99,14 +96,29 @@ public class CLI {
                                                 "\nRemoved Tickets: "+ticketPool.getRemovedTickets()+
                                                 "\n++++++++++++++++");
                             try {
-                                Thread.sleep(4000);
+                                Thread.sleep(4000);//sleeps for 4 seconds
                             } catch (InterruptedException e) {
                                 throw new RuntimeException(e);
                             }
                         }
 
-                        System.out.println("continue:");
-                        ticket_pool_loop = input.nextBoolean();
+                        try
+                        {   System.out.println("continue:(y/n)");  // ask for continue the ticket track loop
+                            String response = input.nextLine();
+                            if(response.equalsIgnoreCase("y")){
+                                ticket_pool_loop = true;
+                            } else if (response.equalsIgnoreCase("n")) {
+                                ticket_pool_loop = false;
+                            }else{
+                                System.out.println("Please enter a valid input...!");
+                                ticket_pool_loop = true;//if the input is invalid loop will continue
+                            }
+                            ticket_pool_loop = input.nextBoolean();
+                        }catch(InputMismatchException e){
+                            System.out.println("Invalid input: "+e.getMessage());
+                            ticket_pool_loop = false;
+                        }
+
                     }
                     break;
 
@@ -148,7 +160,7 @@ public class CLI {
 
         }
         context.close();
-        System.exit(0);
+        System.exit(0);//to close the backend
 
     }
 }
